57ba58362b7a007392062a650d814ec1472287c9
emdickim <kimlazaropardo@gmail.com>
2025-02-15 17:53:31 +0100

    fet

--- python_postgresql/connect.py
+++ python_postgresql/connect.py
@@ -1,11 +1,19 @@
 import psycopg2
 
-def connection_db();
+
+def connection_db():
     conn = psycopg2.connect(
-       database="the_bear",
-       password="admin",
-       user="admin",
-       host="localhost",
-       port="5432" 
+        database="the_bear",
+        password="admin",
+        user="admin",
+        host="localhost",
+        port="5432"
     )
-return conn
+
+    return conn
+
+
+connect = connection_db()
+print(connect)
+connect.close()
+print(connect)
